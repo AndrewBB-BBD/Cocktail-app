c5adfff586b45dac965daf82e6c070613d3178d3
AndrewBB-BBD <andrewb@bbd.co.za>
2022-04-15 12:59:02 +0200

    feat: add dummy users

--- insertDataScript.sql
+++ insertDataScript.sql
@@ -204,6 +204,13 @@ INSERT INTO Recipe (flavourID, difficultyID, typeID, recipeName, recipeMethod, r
 (4, 2, 2, 'Lemon, lime and bitters', 'Rim the inside (and optionally outside) of the glass with 4 to 5 dashes of Angostura Bitters then pour lemonade and lime cordial (15–30 mL) into glass. Garnish with Lemon if desired. Has 0.2% alcohol.', 'https://drive.google.com/file/d/1rCykHeWL-4Jt6l0SI8QTeh3vviGkYv93/view?usp=sharing', 0, 15),
 (2, 1, 2, 'Roy Rogers', 'Mix the cola and grenadine syrup together. Pour into a glass filled with ice and garnish with a cherry.', 'https://drive.google.com/file/d/1DnoVjJ3CBak0LuY_UW3oRS-NCIG3NJLH/view?usp=sharing', 0, 5);
 
+INSERT INTO UserLogin (userEmail, username, userPassword, salt) VALUES
+('test@test.com', 'andrewB', HASHBYTES('SHA2_512', 'AndrewBPassword'), NEWID()),
+('test1@test.com', 'andrewC', HASHBYTES('SHA2_512', 'AndrewCPassword'), NEWID()),
+('test2@test.com', 'andrewD', HASHBYTES('SHA2_512', 'AndrewDPassword'), NEWID()),
+('test3@test.com', 'andrewE', HASHBYTES('SHA2_512', 'AndrewEPassword'), NEWID()),
+('test4@test.com', 'andrewF', HASHBYTES('SHA2_512', 'AndrewFPassword'), NEWID());
+
 INSERT INTO IngredientMeasurement (recipeID, ingredientID, measurementID, measurementAmount) VALUES
 (1, 1, 9, '35'),
 (1, 2, 9, '35'),
