29916524a32a5c6564cadb99b2c7d5b2949efefc
vianka-naidoo <viankan@bbd.co.za>
2022-04-20 12:16:27 +0200

    Implementation of search and filter features for the discover page

--- CocktailApp/Pages/Discover.cshtml.cs
+++ CocktailApp/Pages/Discover.cshtml.cs
@@ -8,59 +8,102 @@ namespace CocktailApp.Pages;
 public class DiscoverModel : PageModel
 {
     private readonly cocktailDBContext _cocktailDBContext;
+    public List<Recipe> recipesList = new List<Recipe>();
+
+    // sets the DB context and list of recipes as well as the recipe types, flavours, difficulties, and times for the filter menu
     public DiscoverModel(cocktailDBContext cocktailDBContext)
     {
         _cocktailDBContext = cocktailDBContext;
+        recipesList = _cocktailDBContext.Recipes.ToList();
+        recipeTypes = _cocktailDBContext.RecipeTypes.ToList();
+        flavourProfiles = _cocktailDBContext.FlavourProfiles.ToList();
+        difficulties = _cocktailDBContext.Difficulties.ToList();
     }
  
-    public List<Recipe> recipesList = new List<Recipe>();
 
     // filter options
     public List<RecipeType> recipeTypes = new List<RecipeType>();
     public List<FlavourProfile> flavourProfiles = new List<FlavourProfile>();
     public List<Difficulty> difficulties = new List<Difficulty>();
 
+    // selected filters
+    public List<string> selectedRecipeTypes = new List<string>();
+    public List<string> selectedFlavourProfiles = new List<string>();
+    public List<string> selectedDifficulties = new List<string>();
+
     public int minTime;
     public int maxTime;
+    public string searchResults = "";
  
-    public async Task<IActionResult> OnGet()
+    public IActionResult OnGet()
     {
-        loadRecipes(new List<string>(), new List<string>(), new List<string>());
-        minTime = _cocktailDBContext.Recipes.Select(r => r.RecipeTime).Min();
-        maxTime = _cocktailDBContext.Recipes.Select(r => r.RecipeTime).Max();
+        loadRecipesAndInfo(recipesList);
         return Page();
     }
 
+    // redirects the user to a recipe page when they select a recipe card
     public IActionResult OnGetViewRecipe(int ID, string URL) {
         return RedirectToPage("Recipe", new {recipeID = ID});
     }
 
+    // executes when the user searches or filters recipes and retrieves the request data
     public void OnPost() {
-        string reqData;
+        string reset = Request.Form["reset"];
+        string searchTerm = Request.Form["searchTerm"];
 
-        // recipe types
-        reqData = Request.Form["recipeType"];
-        List<string> selectedRecipeTypes = new List<string>();
-        if (reqData != null)   
-            selectedRecipeTypes = reqData.Split(',').ToList();
+        if (reset != null) {    // reset 
+            loadRecipesAndInfo(_cocktailDBContext.Recipes.ToList());
+        } else if (!string.IsNullOrEmpty(searchTerm)) {     // search
+            loadRecipesAndInfo(_cocktailDBContext.Recipes.ToList());
+            findRecipes(searchTerm);
+        } else {    // filter
+            string reqData;
 
-        // flavour profiles
-        reqData = Request.Form["flavourProfile"];
-        List<string> selectedFlavourProfiles = new List<string>();
-        if (reqData != null)   
-            selectedFlavourProfiles = reqData.Split(',').ToList();
+            // recipe types
+            reqData = Request.Form["recipeType"];
+            if (reqData != null)   
+                selectedRecipeTypes = reqData.Split(',').ToList();
 
-        // difficulty
-        reqData = Request.Form["difficulty"];
-        List<string> selectedDifficulties = new List<string>();
-        if (reqData != null)   
-            selectedDifficulties = reqData.Split(',').ToList();
+            // flavour profiles
+            reqData = Request.Form["flavourProfile"];
+            if (reqData != null)   
+                selectedFlavourProfiles = reqData.Split(',').ToList();
 
-        loadRecipes(selectedRecipeTypes, selectedFlavourProfiles, selectedDifficulties);   
+            // difficulty
+            reqData = Request.Form["difficulty"];
+            if (reqData != null)   
+                selectedDifficulties = reqData.Split(',').ToList();
+
+            // time
+            reqData = Request.Form["time"];
+            if (reqData != null)
+                filterRecipes(selectedRecipeTypes, selectedFlavourProfiles, selectedDifficulties, int.Parse(reqData), recipesList);
+            else    
+                filterRecipes(selectedRecipeTypes, selectedFlavourProfiles, selectedDifficulties, 0, recipesList);
+        }
     }
 
-    public void loadRecipes(List<string> selectedRecipeTypes, List<string> selectedFlavourProfiles, List<string> selectedDifficulties) {
-        recipesList =  _cocktailDBContext.Recipes.ToList();
+    // loads all recipes and their data
+    public void loadRecipesAndInfo(List<Recipe> recipes) {
+        recipesList =  recipes;
+        minTime = recipesList.Select(r => r.RecipeTime).Min();
+        maxTime = recipesList.Select(r => r.RecipeTime).Max();
+
+        foreach (var recipe in recipesList) {
+            int start = recipe.RecipeImage.IndexOf("/file/d/") + 8;
+            int end = recipe.RecipeImage.IndexOf("/view?usp=sharing");
+            string imageURL = "https://drive.google.com/uc?id=" + recipe.RecipeImage.Substring(start, end - start);
+            recipe.RecipeImage = imageURL;
+        }
+    }
+
+    // filters the recipes displayed according to the user's selection
+    public void filterRecipes(List<string> selectedRecipeTypes, List<string> selectedFlavourProfiles, List<string> selectedDifficulties, int time, List<Recipe> recipes) {
+        
+        if (recipes.Count() > 0)
+            loadRecipesAndInfo(recipes);
+        else
+            loadRecipesAndInfo(_cocktailDBContext.Recipes.ToList());
 
         if (selectedRecipeTypes.Count > 0) {
             recipesList = recipesList.Where(r => selectedRecipeTypes.Select(int.Parse).ToList().Contains(r.TypeId)
@@ -77,15 +120,16 @@ public class DiscoverModel : PageModel
             ).ToList();
         } 
 
-        foreach (var recipe in recipesList) {
-            int start = recipe.RecipeImage.IndexOf("/file/d/") + 8;
-            int end = recipe.RecipeImage.IndexOf("/view?usp=sharing");
-            string imageURL = "https://drive.google.com/uc?id=" + recipe.RecipeImage.Substring(start, end - start);
-            recipe.RecipeImage = imageURL;
-        }
+        if (time > 0)
+            recipesList = recipesList.Where(r => r.RecipeTime <= time).ToList();
 
-        recipeTypes =  _cocktailDBContext.RecipeTypes.ToList();
-        flavourProfiles =  _cocktailDBContext.FlavourProfiles.ToList();
-        difficulties =  _cocktailDBContext.Difficulties.ToList();
+        searchResults = recipesList.Count().ToString() + " recipes found.";
     }
+
+    // searches for a recipe with the specified search term in its name
+    public void findRecipes(string searchTerm) {
+        recipesList =  recipesList.Where(r => r.RecipeName.ToUpper().Contains(searchTerm.ToUpper())).ToList();
+        searchResults = recipesList.Count().ToString() + " results for '" + searchTerm + "'.";
+    }
+
 }
